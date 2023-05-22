de38ef787d0f84e96e1fdb6bbae5873b80602fe5
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-05-22 17:14:54 -0300

    Creating a video streaming endpoint with fs createReadStream

---
+++ README.md
@@ -0,0 +1,19 @@
+# Installation steps
+
++ Clone this repository and navigate into it
++ Copy .env.example to a .env file
+    ```console
+    cp .env.example .env
+    ``` 
++ Install the dependencies
+    ```console
+    npm install
+    ```
+
+# Execution steps
+
++ Start the development server
+    ```console
+    npm run start:dev
+    ```
++ Open the file located at src/index.html in your browser
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import fs from 'fs';
+import path from "path";
 
 dotenv.config();
 
@@ -18,8 +20,35 @@ app.use(express.json());
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    res.json("Hello world!")
+router.get('/video', async (req, res) => {
+    const relativeVideoPath = path.join(__dirname, '../coffee.mp4');
+    const absolutePath = path.resolve(relativeVideoPath);
+    
+    const range = req.headers.range;
+
+    if (!range) {
+        return res.status(400).send("Requires Range header");
+    }
+
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const videoSize = fs.statSync(absolutePath).size;
+
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    const contentLength = end - start + 1;
+
+    const responseHeaders = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+    };
+
+    res.writeHead(206, responseHeaders);
+
+    const videoStream = fs.createReadStream(absolutePath, { start, end });
+    console.log(videoStream);
+    videoStream.pipe(res);
 });
 
 app.use(router);
